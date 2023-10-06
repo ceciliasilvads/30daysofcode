5bf7bfa55655c33fe55b02143d56712d7a585ef9
Cecília Souza <ceciliasilvadsza@gmail.com>
2023-10-06 11:15:28 -0300

    Add polymorphism

--- 8-14/Inheritance.py
+++ 8-14/Inheritance.py
@@ -4,15 +4,24 @@ class Veiculo:
         self.ano = ano
         self.valor = valor
 
+    def Ligar(self):
+        print('O veiculo ligou!')
+
 class Motocicleta(Veiculo):
     def __init__(self, marca: str, ano: str, valor: float, cilindradas: int):
         super().__init__(marca, ano, valor)
         self.cilindradas = cilindradas
+    
+    def Ligar(self):
+        print('A moto ligou!')
 
 class Aviao(Veiculo):
     def __init__(self, marca: str, ano: str, valor: float, turbinas: int):
         super().__init__(marca, ano, valor)
         self.turbinas = turbinas
+
+    def Ligar(self):
+        print('O avião ligou!')
  
 veiculo1 = Veiculo('VW', '2010', 29900)
 moto = Motocicleta('Honda', '2021', 2500, 200)
@@ -20,9 +29,12 @@ junkersf13 = Aviao('None', '1919', 3000000,18)
 
 print(type(veiculo1))
 print(veiculo1.ano)
+veiculo1.Ligar()
 
 print(type(moto))
 print(moto.ano)
+moto.Ligar()
 
 print(type(junkersf13))
 print(junkersf13.ano)
+junkersf13.Ligar()
